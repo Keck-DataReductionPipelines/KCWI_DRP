a0ef445ec90982a14468741ed9598f0752f7934e
Luca Rizzi <luca.rizzi.dr@gmail.com>
2020-04-06 18:32:23 -1000

    Refactor CalcPrelimDisp.py

--- kcwidrp/primitives/CalcPrelimDisp.py
+++ kcwidrp/primitives/CalcPrelimDisp.py
@@ -13,26 +13,26 @@ class CalcPrelimDisp(BasePrimitive):
 
     def _perform(self):
         # get binning
-        ybin = self.action.args.ybinsize
+        y_binning = self.action.args.ybinsize
         # 0 - compute alpha
-        prelim_alpha = self.action.args.grangle - 13.0 - self.action.args.adjang
+        preliminary_alpha = self.action.args.grangle - 13.0 - self.action.args.adjang
         # 1 - compute preliminary angle of diffraction
-        prelim_beta = self.action.args.camangle - prelim_alpha
+        preliminary_beta = self.action.args.camangle - preliminary_alpha
         # 2 - compute preliminary dispersion
-        prelim_disp = math.cos(prelim_beta/math.degrees(1.)) / \
+        preliminary_dispersion = math.cos(preliminary_beta/math.degrees(1.)) / \
             self.action.args.rho / self.config.instrument.FCAM * \
-            (self.config.instrument.PIX*ybin) * 1.e4
-        prelim_disp *= math.cos(
+            (self.config.instrument.PIX*y_binning) * 1.e4
+        preliminary_dispersion *= math.cos(
             self.config.instrument.GAMMA/math.degrees(1.))
         self.logger.info("Initial alpha, beta (deg): %.3f, %.3f" %
-                         (prelim_alpha, prelim_beta))
+                         (preliminary_alpha, preliminary_beta))
         self.logger.info("Initial calculated dispersion (A/binned pix): %.3f" %
-                         prelim_disp)
-        self.context.prelim_disp = prelim_disp
+                         preliminary_dispersion)
+        self.context.prelim_disp = preliminary_dispersion
 
-        logstr = CalcPrelimDisp.__module__ + "." + CalcPrelimDisp.__qualname__
-        self.action.args.ccddata.header['HISTORY'] = logstr
-        self.logger.info(logstr)
+        log_string = CalcPrelimDisp.__module__ + "." + CalcPrelimDisp.__qualname__
+        self.action.args.ccddata.header['HISTORY'] = log_string
+        self.logger.info(log_string)
 
         return self.action.args
     # END: class CalcPrelimDisp()
