3b762a75163afb42e10ebaa85560a0f5634e73cb
Don Neill <neilljd@gmail.com>
2020-04-21 11:27:15 -0700

    Refined handling of buffer region in vignetting correction

--- kcwidrp/primitives/MakeMasterFlat.py
+++ kcwidrp/primitives/MakeMasterFlat.py
@@ -124,7 +124,7 @@ class MakeMasterFlat(BaseImg):
         ffleft = int(10/xbin)
         ffright = int(70/xbin)
 
-        buffer = 5.0/float(xbin)
+        buffer = 6.0/float(xbin)
 
         # reference slice
         refslice = 9
@@ -136,14 +136,16 @@ class MakeMasterFlat(BaseImg):
 
         # correct vignetting if we are using internal flats
         if internal:
+            self.logger.info("Internal flats require vignetting correction")
             # get good region for fitting
             waves = wavemap.data.compress((wavemap.data > 0.).flat)
             waves = [waves.min(), waves.max()]
             dw = (waves[1] - waves[0]) / 30.0
             wavemin = (waves[0]+waves[1]) / 2.0 - dw
             wavemax = (waves[0]+waves[1]) / 2.0 + dw
-            print(wavemin, wavemax)
-
+            self.logger.info("Using %.1f - %.1f A of slice %d" % (wavemin,
+                                                                  wavemax,
+                                                                  refslice))
             # get reference slice data
             q = [i for i, v in enumerate(slicemap.data.flat) if v == refslice]
             xflat = []
@@ -156,19 +158,19 @@ class MakeMasterFlat(BaseImg):
                     yflat.append(stacked.data.flat[i])
                     wflat.append(wavemap.data.flat[i])
                     qq.append(i)
-
+            # get un-vignetted portion
             qflat = [i for i, v in enumerate(xflat) if flatl <= v <= flatr]
             xflat = [xflat[i] for i in qflat]
             yflat = [yflat[i] for i in qflat]
             wflat = [wflat[i] for i in qflat]
-            print(np.min(xflat), np.max(xflat))
+            # sort on wavelength
             sw = np.argsort(wflat)
             ywflat = [yflat[i] for i in sw]
             wwflat = [wflat[i] for i in sw]
+            # fit wavelength slope
             wavelinfit = np.polyfit(wwflat, ywflat, 2)
-            print(wavelinfit)
-            yfit = np.polyval(wavelinfit, wflat)
-
+            wslfit = np.polyval(wavelinfit, wflat)
+            # plot slope fit
             if self.config.instrument.plot_level >= 1:
                 p = figure(title=self.action.args.plotlabel + ' WAVE SLOPE FIT',
                            x_axis_label='wave px',
@@ -176,36 +178,43 @@ class MakeMasterFlat(BaseImg):
                            plot_width=self.config.instrument.plot_width,
                            plot_height=self.config.instrument.plot_height)
                 p.circle(wwflat, ywflat, legend_label="Data")
-                p.line(wflat, yfit, line_color='red', line_width=3,
+                p.line(wflat, wslfit, line_color='red', line_width=3,
                        legend_label="Fit")
                 bokeh_plot(p, self.context.bokeh_session)
                 if self.config.instrument.plot_level >= 2:
                     input("Next? <cr>: ")
                 else:
                     time.sleep(self.config.instrument.plot_pause)
-
-            yflat = yflat / np.polyval(wavelinfit, wflat)
+            # take out slope
+            yflat = yflat / wslfit
+            # now sort on slice position
             ss = np.argsort(xflat)
             xflat = [xflat[i] for i in ss]
             yflat = [yflat[i] for i in ss]
-            wflat = [wflat[i] for i in ss]
+            # fit un-vignetted slope
             resflat = np.polyfit(xflat, yflat, 1)
 
-            # select the point we will fit for the vignetting
+            # select the points we will fit for the vignetting
+            # get reference region
             xfit = [posmap.data.flat[i] for i in qq]
             yfit = [stacked.data.flat[i] for i in qq]
             wflat = [wavemap.data.flat[i] for i in qq]
+            # take out wavelength slope
             yfit = yfit / np.polyval(wavelinfit, wflat)
 
-            # fit the vignetted region
+            # select the vignetted region
             qfit = [i for i, v in enumerate(xfit) if fitl <= v <= fitr]
             xfit = [xfit[i] for i in qfit]
             yfit = [yfit[i] for i in qfit]
+            # sort on slice position
             s = np.argsort(xfit)
             xfit = [xfit[i] for i in s]
             yfit = [yfit[i] for i in s]
+            # fit vignetted slope
             resfit = np.polyfit(xfit, yfit, 1)
-
+            # compute the intersection
+            xinter = -(resflat[1] - resfit[1]) / (resflat[0] - resfit[0])
+            # plot slice profile and fits
             if self.config.instrument.plot_level >= 1:
                 p = figure(title=self.action.args.plotlabel + ' Vignetting',
                            x_axis_label='Slice Pos (px)',
@@ -215,46 +224,79 @@ class MakeMasterFlat(BaseImg):
                 p.circle(posmap.data.flat[qq],
                          stacked.data.flat[qq] / np.polyval(
                              wavelinfit, wavemap.data.flat[qq]))
-                p.line(allidx, resfit[1] + resfit[0]*allidx, line_color='purple')
+                p.line(allidx, resfit[1] + resfit[0]*allidx,
+                       line_color='purple')
                 p.line(allidx, resflat[1] + resflat[0]*allidx, line_color='red')
                 p.line([fitl, fitl], [0.5, 1.5], line_color='blue')
                 p.line([fitr, fitr], [0.5, 1.5], line_color='blue')
                 p.line([flatl, flatl], [0.5, 1.5], line_color='green')
                 p.line([flatr, flatr], [0.5, 1.5], line_color='green')
+                p.line([xinter-buffer, xinter-buffer], [0.5, 1.5],
+                       line_color='black')
+                p.line([xinter + buffer, xinter + buffer], [0.5, 1.5],
+                       line_color='black')
+                p.line([xinter, xinter], [0.5, 1.5], line_color='red')
                 bokeh_plot(p, self.context.bokeh_session)
                 if self.config.instrument.plot_level >= 2:
                     input("Next? <cr>: ")
                 else:
                     time.sleep(self.config.instrument.plot_pause)
-            # compute the intersection
-            xinter = -(resflat[1]-resfit[1])/(resflat[0]-resfit[0])
+
             # figure out where the correction applies
-            qinter = [i for i, v in enumerate(posmap.data.flat)
-                      if 0 <= v <= (xinter-buffer)]
+            qcor = [i for i, v in enumerate(posmap.data.flat)
+                    if 0 <= v <= (xinter-buffer)]
             # apply the correction!
-            for i in qinter:
+            self.logger.info("Applying vignetting correction...")
+            for i in qcor:
                 newflat.flat[i] = (resflat[1]+resflat[0]*posmap.data.flat[i]) \
                                 / (resfit[1]+resfit[0]*posmap.data.flat[i]) * \
                                 stacked.data.flat[i]
             # now deal with the intermediate (buffer) region
-            qspline = [i for i, v in enumerate(posmap.data.flat)
-                       if (xinter-buffer) <= v <= (xinter+buffer)]
-            xspline = [posmap.data.flat[i] for i in qspline]
-            posmin = np.min(xspline)
-            posmax = np.max(xspline)
-            valuemin = (resflat[1]+resflat[0]*posmin) / \
-                       (resfit[1]+resfit[0]*posmin)
-            valuemax = 1
-            cs = CubicSpline([posmin, posmax], [valuemin, valuemax])
-            for i in qspline:
-                newflat.flat[i] = cs(posmap.data.flat[i]) * newflat.flat[i]
+            self.logger.info("Done, now handling buffer region")
+            # get buffer points to fit in reference region
+            qbff = [i for i in qq if (xinter-buffer) <=
+                    posmap.data.flat[i] <= (xinter+buffer)]
+            # get slice pos and data for buffer fitting
+            xbuff = [posmap.data.flat[i] for i in qbff]
+            ybuff = [stacked.data.flat[i] / np.polyval(wavelinfit,
+                                                       wavemap.data.flat[i])
+                     for i in qbff]
+            # sort on slice position
+            sp = np.argsort(xbuff)
+            xbuff = [xbuff[i] for i in sp]
+            ybuff = [ybuff[i] for i in sp]
+            # fit buffer with low-order poly
+            buffit = np.polyfit(xbuff, ybuff, 3)
+            # plot buffer fit
+            if self.config.instrument.plot_level >= 1:
+                p = figure(title=self.action.args.plotlabel + ' Buffer Region',
+                           x_axis_label='Slice Pos (px)',
+                           y_axis_label='Ratio',
+                           plot_width=self.config.instrument.plot_width,
+                           plot_height=self.config.instrument.plot_height)
+                p.circle(xbuff, ybuff)
+                p.line(xbuff, np.polyval(buffit, xbuff), line_color='red')
+                bokeh_plot(p, self.context.bokeh_session)
+                if self.config.instrument.plot_level >= 2:
+                    input("Next? <cr>: ")
+                else:
+                    time.sleep(self.config.instrument.plot_pause)
+            # get all buffer points in image
+            qbuf = [i for i, v in enumerate(posmap.data.flat)
+                    if (xinter-buffer) <= v <= (xinter+buffer)]
+            # apply buffer correction to all buffer points in newflat
+            for i in qbuf:
+                newflat.flat[i] = \
+                    (resflat[1] + resflat[0] * posmap.data.flat[i]) / \
+                    np.polyval(buffit, posmap.data.flat[i]) * newflat.flat[i]
+            self.logger.info("Vignetting correction complete.")
 
         stacked.data = newflat
         # get master flat output name
         mfname = stack_list[0].split('.fits')[0] + '_' + suffix + '.fits'
 
         log_string = MakeMasterFlat.__module__ + "." + \
-                     MakeMasterFlat.__qualname__
+            MakeMasterFlat.__qualname__
         stacked.header['IMTYPE'] = self.action.args.new_type
         stacked.header['HISTORY'] = log_string
         stacked.header['WAVMAPF'] = wmf
