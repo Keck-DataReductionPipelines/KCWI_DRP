5818c7350f1a8f8ec3b7ab04bbbfda4282709dcc
Don Neill <neilljd@gmail.com>
2020-05-01 17:26:22 -0700

    Implement ability to specify non-local sky image and beginning of specifying mask image in SubtractSky.py (working).

--- kcwidrp/primitives/SubtractSky.py
+++ kcwidrp/primitives/SubtractSky.py
@@ -15,17 +15,44 @@ class SubtractSky(BasePrimitive):
         Checks if a master sky exists to subtract
         :return:
         """
-        self.logger.info("Checking precondition for CorrectIllumination")
-        target_type = 'SKY'
-        tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
-                                             target_type=target_type,
-                                             nearest=True)
-        self.logger.info("pre condition got %d master sky, expected 1"
-                         % len(tab))
-        if len(tab) <= 0:
-            return False
-        else:
+        self.logger.info("Checking precondition for SubtractSky")
+        skyfile = None
+        skymask = None
+        # check if kcwi.sky exists
+        if os.path.exists('kcwi.sky'):
+            f = open('kcwi.sky')
+            skyproc = f.readlines()
+            f.close()
+            # is our file in the list?
+            ofn = self.action.args.ccddata.header['OFNAME']
+            for row in skyproc:
+                if ofn in row.split()[0]:
+                    skyfile = row.split()[1]
+                    if len(row.split()) > 2:
+                        skymask = row.split()[2]
+            if skyfile:
+                if not os.path.exists(skyfile):
+                    skyfile = None
+            if skymask:
+                if not os.path.exists(skymask):
+                    skymask = None
+        self.action.args.skyfile = skyfile
+        self.action.args.skymask = skymask
+        if skyfile:
+            self.logger.info("pre condition got 1 master sky, expected 1")
             return True
+        else:
+
+            target_type = 'SKY'
+            tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
+                                                 target_type=target_type,
+                                                 nearest=True)
+            self.logger.info("pre condition got %d master sky, expected 1"
+                             % len(tab))
+            if len(tab) <= 0:
+                return False
+            else:
+                return True
 
     def _perform(self):
         self.logger.info("Subtracting sky background")
@@ -35,26 +62,46 @@ class SubtractSky(BasePrimitive):
         keycom = 'sky corrected?'
         target_type = 'SKY'
 
-        tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
-                                             target_type=target_type,
-                                             nearest=True)
-        self.logger.info("%d master sky frames found" % len(tab))
+        skyfile = self.action.args.skyfile
 
-        if len(tab) > 0:
-            msname = tab['OFNAME'][0].split('.')[0] + '_' + \
-                     target_type.lower() + ".fits"
+        if not self.action.args.skyfile:
+            tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
+                                                 target_type=target_type,
+                                                 nearest=True)
+            self.logger.info("%d master sky frames found" % len(tab))
+
+            if len(tab) > 0:
+                skyfile = tab['OFNAME'][0]
+
+        msname = skyfile.split('.')[0] + '_' + target_type.lower() + ".fits"
+        if os.path.exists(os.path.join(os.path.dirname(self.action.args.name),
+                                       'redux', msname)):
             self.logger.info("Reading image: %s" % msname)
             msky = kcwi_fits_reader(
                 os.path.join(os.path.dirname(self.action.args.name), 'redux',
                              msname))[0]
 
+            # scale the sky?
+            obtime = self.action.args.ccddata.header['XPOSURE']
+            sktime = msky.header['XPOSURE']
+
+            if obtime <= 0. or sktime <= 0.:
+                self.logger.warning("Bad exposure times (obj, sky): %.1f, %1f"
+                                    % (obtime, sktime))
+                skscl = 1.
+            else:
+                skscl = obtime / sktime
+            self.logger.info("Sky scale factor = %.3f" % skscl)
+
             # do the subtraction
-            self.action.args.ccddata.data -= msky.data
+            self.action.args.ccddata.data -= msky.data * skscl
 
             # update header keywords
             self.action.args.ccddata.header[key] = (True, keycom)
             self.action.args.ccddata.header['SKYMAST'] = (msname,
                                                           "Master sky filename")
+            self.action.args.ccddata.header['SKYSCL'] = (skscl,
+                                                         'sky scale factor')
 
         else:
             # update header keywords
