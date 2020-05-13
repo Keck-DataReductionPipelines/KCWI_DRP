cb520998bc47fe8c7d63f60bb396f38834f1be93
Don Neill <neilljd@gmail.com>
2020-05-13 10:41:31 -0700

    Handle twilight and dome flats.

--- kcwidrp/primitives/CorrectIllumination.py
+++ kcwidrp/primitives/CorrectIllumination.py
@@ -17,48 +17,64 @@ class CorrectIllumination(BasePrimitive):
         Checks if we can correct illumination based on the processing table
         :return:
         """
+        self.action.args.master_flat = None
         self.logger.info("Checking precondition for CorrectIllumination")
+        # first check for internal flat
         target_type = 'MFLAT'
         tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
                                              target_type=target_type,
                                              nearest=True)
-        self.logger.info("pre condition got %d master flats, expected 1"
-                         % len(tab))
         if len(tab) <= 0:
-            return False
+            # next look for twilight flat
+            target_type = 'MTWIF'
+            tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
+                                                 target_type=target_type,
+                                                 nearest=True)
+            if len(tab) <= 0:
+                # finally look for dome flat
+                target_type = 'MDOME'
+                tab = self.context.proctab.n_proctab(
+                    frame=self.action.args.ccddata,
+                    target_type=target_type,
+                    nearest=True)
+                if len(tab) <= 0:
+                    precondition = False
+                else:
+                    precondition = True
+            else:
+                precondition = True
         else:
-            return True
+            precondition = True
+
+        self.logger.info("pre condition got %d %s flats, expected 1"
+                         % (len(tab), target_type))
+        if precondition:
+            self.action.args.master_flat = tab['OFNAME'][0].split('.')[0] + \
+                                           '_' + target_type.lower() + ".fits"
+        return precondition
 
     def _perform(self):
 
         # Header keyword to update
         key = 'FLATCOR'
         keycom = 'flat corrected?'
-        target_type = 'MFLAT'
 
         self.logger.info("Correcting Illumination")
-        tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
-                                             target_type=target_type,
-                                             nearest=True)
-        self.logger.info("%d master flat frames found" % len(tab))
-
-        if len(tab) > 0:
-            mfname = tab['OFNAME'][0].split('.')[0] + '_' + \
-                     target_type.lower() + ".fits"
-            self.logger.info("Reading image: %s" % mfname)
+        if self.action.args.master_flat:
             mflat = kcwi_fits_reader(
                 os.path.join(os.path.dirname(self.action.args.name), 'redux',
-                             mfname))[0]
+                             self.action.args.master_flat))[0]
 
             # do the subtraction
             self.action.args.ccddata.data *= mflat.data
 
             # update header keywords
             self.action.args.ccddata.header[key] = (True, keycom)
-            self.action.args.ccddata.header['MFFILE'] = (mfname,
-                                                         "Master flat filename")
+            self.action.args.ccddata.header['MFFILE'] = (
+                self.action.args.master_flat, "Master flat filename")
         else:
-
+            self.logger.error("No master flat found, "
+                              "cannot correct illumination.")
             self.action.args.ccddata.header[key] = (False, keycom)
 
         log_string = CorrectIllumination.__module__
