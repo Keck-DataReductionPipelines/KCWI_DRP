6b7cb561a8eed5dff30a558f6bad9439168dd26d
Don Neill <neilljd@gmail.com>
2020-05-19 15:24:42 -0700

    Next implementation of nod-and-shuffle subtraction. Tested and working, but needs more work on uncertainties and flags.

--- kcwidrp/primitives/NandshuffSubtractSky.py
+++ kcwidrp/primitives/NandshuffSubtractSky.py
@@ -1,7 +1,12 @@
 from keckdrpframework.primitives.base_primitive import BasePrimitive
 from kcwidrp.primitives.kcwi_file_primitives import kcwi_fits_reader, \
     kcwi_fits_writer
+
+import numpy as np
 import os
+
+from astropy.nddata import CCDData
+from astropy import units as u
 
 
 class NandshuffSubtractSky(BasePrimitive):
@@ -12,7 +17,7 @@ class NandshuffSubtractSky(BasePrimitive):
 
     def _pre_condition(self):
         """
-        Checks if a master sky exists to subtract
+        Checks if it is a nod-and-shuffle observation
         :return:
         """
         self.logger.info("Checking precondition for NandshuffSubtractSky")
@@ -24,60 +29,147 @@ class NandshuffSubtractSky(BasePrimitive):
             return False
 
     def _perform(self):
-        self.logger.info("Subtracting sky background")
+        self.logger.info("Subtracting nod-and shuffle sky background")
 
         # Header keyword to update
         key = 'NASSUB'
         keycom = 'Nod-and-shuffle subtraction done?'
         target_type = 'SKY'
 
-        skyfile = self.action.args.skyfile
+        # get header values
+        ofn = self.action.args.ccddata.header['OFNAME']
+        shrows = self.action.args.ccddata.header['SHUFROWS']
+        nshfup = self.action.args.ccddata.header['NSHFUP']
+        nshfdn = self.action.args.ccddata.header['NSHFDN']
 
-        if not self.action.args.skyfile:
-            tab = self.context.proctab.n_proctab(frame=self.action.args.ccddata,
-                                                 target_type=target_type,
-                                                 nearest=True)
-            self.logger.info("%d master sky frames found" % len(tab))
+        # units
+        u_out = self.action.args.ccddata.unit
 
-            if len(tab) > 0:
-                skyfile = tab['OFNAME'][0]
+        # nominal conditions (sky on bottom, object in middle)
+        skyrow0 = 0
+        skyrow1 = shrows - 1
+        objrow0 = shrows
+        objrow1 = shrows + shrows - 1
 
-        msname = skyfile.split('.')[0] + '_' + target_type.lower() + ".fits"
-        if os.path.exists(os.path.join(os.path.dirname(self.action.args.name),
-                                       'redux', msname)):
-            self.logger.info("Reading image: %s" % msname)
-            msky = kcwi_fits_reader(
-                os.path.join(os.path.dirname(self.action.args.name), 'redux',
-                             msname))[0]
+        # aborted script with inverted panels (sky in middle, object above)
+        if nshfdn != nshfup + 1:
+            skyrow0 = shrows
+            skyrow1 = shrows + shrows - 1
+            objrow0 = skyrow1
+            objrow1 = objrow0 + shrows - 1
 
-            # scale the sky?
-            obtime = self.action.args.ccddata.header['XPOSURE']
-            sktime = msky.header['XPOSURE']
+        # check limits
+        if (skyrow1-skyrow0) != (objrow1-objrow0):
+            self.logger.error("Nod-and-shuffle row limits error")
+            return self.action.args
 
-            if obtime <= 0. or sktime <= 0.:
-                self.logger.warning("Bad exposure times (obj, sky): %.1f, %1f"
-                                    % (obtime, sktime))
-                skscl = 1.
-            else:
-                skscl = obtime / sktime
-            self.logger.info("Sky scale factor = %.3f" % skscl)
+        # create intermediate images and headers
+        sky = self.action.args.ccddata.data.copy()
+        obj = self.action.args.ccddata.data.copy()
+        std = self.action.args.ccddata.uncertainty.array.copy()
+        skyhdr = self.action.args.ccddata.header.copy()
+        objhdr = self.action.args.ccddata.header.copy()
 
-            # do the subtraction
-            self.action.args.ccddata.data -= msky.data * skscl
-
-            # update header keywords
-            self.action.args.ccddata.header[key] = (True, keycom)
-            self.action.args.ccddata.header['SKYMAST'] = (msname,
-                                                          "Master sky filename")
-            self.action.args.ccddata.header['SKYSCL'] = (skscl,
-                                                         'sky scale factor')
-
+        # nominal condition
+        if skyrow0 < 10:
+            self.logger.info("standard nod-and-shuffle configuration")
+            skystd = self.action.args.ccddata.uncertainty.array.copy()
+            # skymsk
+            # move sky to object position
+            sky[objrow0:objrow1, :] = obj[skyrow0:skyrow1, :]
+            skystd[objrow0:objrow1, :] = std[skyrow0:skyrow1, :]
+            # skymsk same
+            # do subtraction
+            self.action.args.ccddata.data -= sky
+            self.action.args.ccddata.uncertainty.array = np.sqrt(std ** 2 +
+                                                                 skystd ** 2)
+            # sum with skymsk
+            # clean images
+            self.action.args.ccddata.data[skyrow0:skyrow1, :] = 0.
+            self.action.args.ccddata.data[(objrow1+1):-1, :] = 0.
+            self.action.args.ccddata.uncertainty.array[skyrow0:skyrow1, :] = 0.
+            self.action.args.ccddata.uncertainty.array[(objrow1+1):-1, :] = 0.
+            # similar with mask
+            sky[skyrow0:skyrow1, :] = 0.
+            sky[(objrow1+1), :] = 0.
+            obj[skyrow0:skyrow1, :] = 0.
+            obj[(objrow1+1), :] = 0.
         else:
-            # update header keywords
-            self.action.args.ccddata.header[key] = (False, keycom)
+            self.logger.warning("non-standard nod-and-shuffle configuration")
+            skyscl = -1.
+            while skyscl < 0.:
+                if self.config.instrument.plot_level >= 2:
+                    q = input("Enter scale factor for sky to match obj "
+                              "(float): ")
+                    try:
+                        skyscl = float(q)
+                    except ValueError:
+                        self.logger.warning("Invalid input: %s, try again" % q)
+                        skyscl = -1.0
+                else:
+                    skyscl = 1.0
+            self.logger.info("Sky scaling used = %.2f" % skyscl)
+            objstd = self.action.args.ccddata.uncertainty.array.copy()
+            # objmsk
+            # move object to sky position
+            obj[skyrow0:skyrow1, :] = obj[objrow0:objrow1, :]
+            objstd[skyrow0:skyrow1, :] = std[objrow0:objrow1, :]
+            # objmsk same
+            # do subtraction
+            sky *= skyscl
+            self.action.args.ccddata.data = obj - sky
+            self.action.args.ccddata.uncertainty.array = np.sqrt(std ** 2 +
+                                                                 objstd ** 2)
+            # sum with objmsk
+            # clean images
+            self.action.args.ccddata.data[objrow0:objrow1, :] = 0.
+            self.action.args.ccddata.data[0:skyrow0, :] = 0.
+            self.action.args.ccddata.uncertainty.array[objrow0:objrow1, :] = 0.
+            self.action.args.ccddata.uncertainty.array[0:skyrow0, :] = 0.
+            # similar with mask
+            sky[objrow0:objrow1, :] = 0.
+            sky[0:skyrow0, :] = 0.
+            obj[objrow0:objrow1, :] = 0.
+            obj[0:skyrow0, :] = 0.
+            cmnt = 'Aborted nod-and-shuffle observations'
+            objhdr['COMMENT'] = cmnt
+            skyhdr['COMMENT'] = cmnt
+            self.action.args.ccddata.header['COMMENT'] = cmnt
+            skyhdr['NASSCL'] = (skyscl, 'Scale factor applied to sky panel')
+            self.action.args.ccddata.header['NASSCL'] = (
+                skyscl, 'Scale factor applied to sky panel')
 
+        # log
+        self.logger.info("nod-and-shuffle subtracted, rows (sky0, 1, obj0,1): "
+                         "%d, %d, %d, %d" % (skyrow0, skyrow1,
+                                             objrow0, objrow1))
+        # update headers
         log_string = NandshuffSubtractSky.__module__
+
+        objhdr[key] = (False, keycom)
+        objhdr['HISTORY'] = log_string
+
+        skyhdr[key] = (False, keycom)
+        skyhdr['SKYOBS'] = (True, 'Sky observation?')
+        skyhdr['HISTORY'] = log_string
+
+        self.action.args.ccddata.header[key] = (True, keycom)
         self.action.args.ccddata.header['HISTORY'] = log_string
+
+        # write out sky image
+        msname = ofn.split('.')[0] + '_' + target_type.lower() + '.fits'
+        out_sky = CCDData(sky, meta=skyhdr, unit=u_out)
+        kcwi_fits_writer(out_sky, output_file=msname,
+                         output_dir=self.config.instrument.output_directory)
+        # write out object image
+        obname = ofn.split('.')[0] + '_obj.fits'
+        out_obj = CCDData(obj, meta=objhdr, unit=u_out)
+        kcwi_fits_writer(out_obj, output_file=obname,
+                         output_dir=self.config.instrument.output_directory)
+
+        # update header keywords
+        self.action.args.ccddata.header['SKYMAST'] = (msname,
+                                                      "Master sky filename")
 
         # write out int image
         kcwi_fits_writer(self.action.args.ccddata,
