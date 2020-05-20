2bdb8d76dea6140bd2941cb6b3525c4189c30254
Don Neill <neilljd@gmail.com>
2020-05-20 10:23:49 -0700

    Properly handle mask and flags in NandshuffSubtractSky.py

--- kcwidrp/primitives/NandshuffSubtractSky.py
+++ kcwidrp/primitives/NandshuffSubtractSky.py
@@ -25,7 +25,8 @@ class NandshuffSubtractSky(BasePrimitive):
             self.logger.info("Preconditions for NandshuffSubtractSky met.")
             return True
         else:
-            self.logger.warning("Precondition not met.")
+            self.logger.warning("Precondition not met: "
+                                "not a nod-and-shuffle observation.")
             return False
 
     def _perform(self):
@@ -67,6 +68,8 @@ class NandshuffSubtractSky(BasePrimitive):
         sky = self.action.args.ccddata.data.copy()
         obj = self.action.args.ccddata.data.copy()
         std = self.action.args.ccddata.uncertainty.array.copy()
+        msk = self.action.args.ccddata.mask.copy()
+        flg = self.action.args.ccddata.flags.copy()
         skyhdr = self.action.args.ccddata.header.copy()
         objhdr = self.action.args.ccddata.header.copy()
 
@@ -74,22 +77,28 @@ class NandshuffSubtractSky(BasePrimitive):
         if skyrow0 < 10:
             self.logger.info("standard nod-and-shuffle configuration")
             skystd = self.action.args.ccddata.uncertainty.array.copy()
-            # skymsk
+            skymsk = self.action.args.ccddata.mask.copy()
+            skyflg = self.action.args.ccddata.flags.copy()
             # move sky to object position
             sky[objrow0:objrow1, :] = obj[skyrow0:skyrow1, :]
             skystd[objrow0:objrow1, :] = std[skyrow0:skyrow1, :]
-            # skymsk same
+            skymsk[objrow0:objrow1, :] = msk[skyrow0:skyrow1, :]
+            skyflg[objrow0:objrow1, :] = flg[skyrow0:skyrow1, :]
             # do subtraction
             self.action.args.ccddata.data -= sky
             self.action.args.ccddata.uncertainty.array = np.sqrt(std ** 2 +
                                                                  skystd ** 2)
-            # sum with skymsk
+            self.action.args.ccddata.mask += skymsk
+            self.action.args.ccddata.flags |= skyflg
             # clean images
             self.action.args.ccddata.data[skyrow0:skyrow1, :] = 0.
             self.action.args.ccddata.data[(objrow1+1):-1, :] = 0.
             self.action.args.ccddata.uncertainty.array[skyrow0:skyrow1, :] = 0.
             self.action.args.ccddata.uncertainty.array[(objrow1+1):-1, :] = 0.
-            # similar with mask
+            self.action.args.ccddata.mask[skyrow0:skyrow1, :] = 1
+            self.action.args.ccddata.mask[(objrow1 + 1):-1, :] = 1
+            self.action.args.ccddata.flags[skyrow0:skyrow1, :] = 64
+            self.action.args.ccddata.flags[(objrow1 + 1):-1, :] = 64
             sky[skyrow0:skyrow1, :] = 0.
             sky[(objrow1+1), :] = 0.
             obj[skyrow0:skyrow1, :] = 0.
@@ -110,23 +119,29 @@ class NandshuffSubtractSky(BasePrimitive):
                     skyscl = 1.0
             self.logger.info("Sky scaling used = %.2f" % skyscl)
             objstd = self.action.args.ccddata.uncertainty.array.copy()
-            # objmsk
+            objmsk = self.action.args.ccddata.mask.copy()
+            objflg = self.action.args.ccddata.flags.copy()
             # move object to sky position
             obj[skyrow0:skyrow1, :] = obj[objrow0:objrow1, :]
             objstd[skyrow0:skyrow1, :] = std[objrow0:objrow1, :]
-            # objmsk same
+            objmsk[skyrow0:skyrow1, :] = msk[objrow0:objrow1, :]
+            objflg[skyrow0:skyrow1, :] = flg[objrow0:objrow1, :]
             # do subtraction
             sky *= skyscl
             self.action.args.ccddata.data = obj - sky
             self.action.args.ccddata.uncertainty.array = np.sqrt(std ** 2 +
                                                                  objstd ** 2)
-            # sum with objmsk
+            self.action.args.ccddata.mask += objmsk
+            self.action.args.ccddata.flags |= objflg
             # clean images
             self.action.args.ccddata.data[objrow0:objrow1, :] = 0.
             self.action.args.ccddata.data[0:skyrow0, :] = 0.
             self.action.args.ccddata.uncertainty.array[objrow0:objrow1, :] = 0.
             self.action.args.ccddata.uncertainty.array[0:skyrow0, :] = 0.
-            # similar with mask
+            self.action.args.ccddata.mask[objrow0:objrow1, :] = 1
+            self.action.args.ccddata.mask[0:skyrow0, :] = 1
+            self.action.args.ccddata.flags[objrow0:objrow1, :] = 64
+            self.action.args.ccddata.flags[0:skyrow0, :] = 64
             sky[objrow0:objrow1, :] = 0.
             sky[0:skyrow0, :] = 0.
             obj[objrow0:objrow1, :] = 0.
