ccd1ff4cce9d864b4324a2dd466e9cc8f521a2ff
Don Neill <neilljd@gmail.com>
2020-05-21 09:24:04 -0700

    Added delta wavelength map to geometry maps in GenerateMaps.py: needed for more accurate efficiency calculation downstream.

--- kcwidrp/primitives/GenerateMaps.py
+++ kcwidrp/primitives/GenerateMaps.py
@@ -18,42 +18,49 @@ class GenerateMaps(BasePrimitive):
     def _perform(self):
         self.logger.info("Generating geometry maps")
 
+        log_string = GenerateMaps.__module__
+
         if self.action.args.geometry_file is not None and \
                 os.path.exists(self.action.args.geometry_file):
             with open(self.action.args.geometry_file, 'rb') as ifile:
                 geom = pickle.load(ifile)
             # get geom params
-            xl0s = geom['xl0']
-            xl1s = geom['xl1']
+            xl0s = geom['xl0']  # lower slice pos limit
+            xl1s = geom['xl1']  # upper slice pos limit
             invtf_list = geom['invtf']
             wave0 = geom['wave0out']
             dw = geom['dwout']
             xsize = geom['xsize']
             # Store original data
             data_img = self.action.args.ccddata.data
-            ny = data_img.shape[0]
+            ny = data_img.shape[0]  # number of wavelength pixels
             # Create map images
             wave_map_img = np.full_like(data_img, fill_value=-1.)
             xpos_map_img = np.full_like(data_img, fill_value=-1.)
             slice_map_img = np.full_like(data_img, fill_value=-1.)
+            delta_map_img = np.full_like(data_img, fill_value=-1.)
             # loop over slices
             for isl in range(0, 24):
                 itrf = invtf_list[isl]
                 xl0 = xl0s[isl]
                 xl1 = xl1s[isl]
+                # loop over slice position
                 for ix in range(xl0, xl1):
                     coords = np.zeros((ny, 2))
+                    # loop over wavelength pixels
                     for iy in range(0, ny):
                         coords[iy, 0] = ix - xl0
                         coords[iy, 1] = iy
                     ncoo = itrf(coords)
+                    # loop over wavelength
                     for iy in range(0, ny):
                         if 0 <= ncoo[iy, 0] <= xsize:
                             slice_map_img[iy, ix] = isl
                             xpos_map_img[iy, ix] = ncoo[iy, 0]
                             wave_map_img[iy, ix] = ncoo[iy, 1] * dw + wave0
-
-            log_string = GenerateMaps.__module__
+                            if iy > 0:
+                                delta_map_img[iy, ix] = abs(
+                                    (ncoo[iy, 1] - ncoo[iy-1, 1]) * dw)
 
             # update header
             self.action.args.ccddata.header['HISTORY'] = log_string
@@ -113,6 +120,11 @@ class GenerateMaps(BasePrimitive):
                              output_file=self.action.args.name,
                              output_dir=self.config.instrument.output_directory,
                              suffix="slicemap")
+            kcwi_fits_writer(CCDData(delta_map_img, meta=header,
+                                     unit=u.angstrom),
+                             output_file=self.action.args.name,
+                             output_dir=self.config.instrument.output_directory,
+                             suffix="delmap")
 
         else:
             self.logger.error("Geom file not accessible")
