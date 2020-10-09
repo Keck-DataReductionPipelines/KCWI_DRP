3f4fcf4008886e1ed23568af4f24c37bbcee9900
Don Neill <neilljd@gmail.com>
2020-10-09 09:27:05 -0700

    fix install_requires list (giving up)

--- setup.py
+++ setup.py
@@ -46,5 +46,5 @@ setup(name=NAME,
       packages=find_packages(),
       scripts=scripts,
       entry_points=entry_points,
-      install_requires=options["install_requires"].split(',')
+      install_requires=['ccdproc', 'bokeh', 'numpy', 'scipy', 'astropy']
       )
