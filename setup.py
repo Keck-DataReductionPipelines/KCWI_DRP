0bcebbbfae43b3e52a129ced4f13d5e48cb95b53
Don Neill <neilljd@gmail.com>
2020-10-09 09:13:35 -0700

    Used metadata and options in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #
 # Licensed under a 3-clause BSD license.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Get some values from the setup.cfg
 try:
@@ -13,11 +13,16 @@ except ImportError:
 
 conf = ConfigParser()
 conf.read(['setup.cfg'])
-# metadata = dict(conf.items('metadata'))
+metadata = dict(conf.items("metadata"))
+options = dict(conf.items("options"))
 
 NAME = 'kcwidrp'
 VERSION = '0.11.1dev'
 RELEASE = 'dev' not in VERSION
+AUTHOR = metadata["author"]
+AUTHOR_EMAIL = metadata["author_email"]
+LICENSE = metadata["license"]
+DESCRIPTION = metadata["description"]
 
 # scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
 #            if os.path.basename(fname) != 'README.rst']
@@ -27,19 +32,19 @@ entry_points = {
     'console_scripts': [
         "reduce_kcwi = kcwidrp.scripts.reduce_kcwi:main",
         "kcwi_masksky_ds9 = kcwidrp.scripts.kcwi_masksky_ds9:main"
-        # "smart_reduce_kcwi = kcwidrp.scripts.smart_reduce_kcwi:main"
+        "smart_reduce_kcwi = kcwidrp.scripts.smart_reduce_kcwi:main"
     ]}
 
 setup(name=NAME,
       provides=NAME,
       version=VERSION,
-      license='BSD3',
-      description='KCWI DRP',
+      license=LICENSE,
+      description=DESCRIPTION,
       long_description=open('README.rst').read(),
-      author='Don Neill',
-      author_email='neilljd@gmail.com',
-      packages=['kcwidrp', ],
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      packages=find_packages(),
       scripts=scripts,
-      entry_points=entry_points, install_requires=['ccdproc', 'bokeh', 'numpy',
-                                                   'scipy', 'astropy']
+      entry_points=entry_points,
+      install_requires=options["install_requires"]
       )
