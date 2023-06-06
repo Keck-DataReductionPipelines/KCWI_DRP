8065d7a890505f548b7cb63c98c9e760bb12a953
Don Neill <neilljd@gmail.com>
2023-06-05 14:22:35 -1000

    Align Red to Blue with 180 degree rotation of cubes in MakeCube.py (second try)

--- kcwidrp/primitives/MakeCube.py
+++ kcwidrp/primitives/MakeCube.py
@@ -228,16 +228,16 @@ class MakeCube(BasePrimitive):
 
             # Rotate RED data by 180 to align with Blue
             if 'RED' in self.action.args.ccddata.header['CAMERA'].upper():
-                out_cube = np.rot90(out_cube, 2)
-                out_vube = np.rot90(out_vube, 2)
-                out_mube = np.rot90(out_mube, 2)
-                out_fube = np.rot90(out_fube, 2)
+                out_cube = np.rot90(out_cube, 2, (1, 2))
+                out_vube = np.rot90(out_vube, 2, (1, 2))
+                out_mube = np.rot90(out_mube, 2, (1, 2))
+                out_fube = np.rot90(out_fube, 2, (1, 2))
                 if obj is not None:
-                    out_oube = np.rot90(out_oube, 2)
+                    out_oube = np.rot90(out_oube, 2, (1, 2))
                 if sky is not None:
-                    out_sube = np.rot90(out_sube, 2)
+                    out_sube = np.rot90(out_sube, 2, (1, 2))
                 if dew is not None:
-                    out_dube = np.rot90(out_dube, 2)
+                    out_dube = np.rot90(out_dube, 2, (1, 2))
 
             # Calculate some WCS parameters
             # Get object pointing
