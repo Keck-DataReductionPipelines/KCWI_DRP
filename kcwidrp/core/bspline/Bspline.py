11ba585c2b926e600c0131ef3ff4efeab3d55425
Don Neill <neilljd@gmail.com>
2023-06-22 11:07:46 -0700

    Fix a possible bug in Bspline.py

--- kcwidrp/core/bspline/Bspline.py
+++ kcwidrp/core/bspline/Bspline.py
@@ -430,7 +430,7 @@ class Bspline(object):
         nbkpt = self.mask.sum()
         if nbkpt <= 2*self.nord:
             return -2
-        hmm = err[np.unique(err/self.npoly)]/self.npoly
+        hmm = err[np.unique(err)]/self.npoly
         n = nbkpt - self.nord
         if np.any(hmm >= n):
             return -2
