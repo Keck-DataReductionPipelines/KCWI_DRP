491922484381944ab942fb0c0ee6264dd012db03
Don Neill <neilljd@gmail.com>
2023-09-14 16:23:26 -0700

    Rename ApplyFlat.py as Template.py as it was never implemented and serves as a better example primitive

--- kcwidrp/primitives/Template.py
+++ kcwidrp/primitives/Template.py
@@ -1,2 +1,23 @@
 from keckdrpframework.primitives.base_primitive import BasePrimitive
 from keckdrpframework.models.arguments import Arguments
+
+
+class Template(BasePrimitive):
+    """
+    Generic template primitive routine.
+    """
+
+    def __init__(self, action, context):
+        BasePrimitive.__init__(self, action, context)
+        self.logger = context.pipeline_logger
+
+    def _perform(self):
+        self.logger.info("This is a Template primitive")
+
+        log_string = Template.__module__ + "." + Template.__qualname__
+        self.action.args.ccddata.header['HISTORY'] = log_string
+        self.logger.info(log_string)
+
+        return self.action.args
+    # END: class Template()
+
