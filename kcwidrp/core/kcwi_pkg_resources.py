14a4e84829775e6a2b77fb6167254b5e0e4d5112
Max Brodheim <max.brodheim@gmail.com>
2026-02-11 13:57:22 -1000

    remove filenotfound check in package fetch

--- kcwidrp/core/kcwi_pkg_resources.py
+++ kcwidrp/core/kcwi_pkg_resources.py
@@ -5,7 +5,4 @@ def get_resource_path(pkg, path):
     """Get the full path to a resource file within a package."""
     ref = importlib_resources.files(pkg) / path
     with importlib_resources.as_file(ref) as full_path:
-        if os.path.exists(full_path):
-            return str(full_path)
-        else:
-            raise FileNotFoundError(f"Resource file {path} not found in package {pkg}.")
+        return str(full_path)
