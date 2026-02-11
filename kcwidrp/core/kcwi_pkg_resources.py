3de8ff1d178ffed4e3c8c50b3e132aca9e1d2496
Max Brodheim <max.brodheim@gmail.com>
2026-02-11 13:18:37 -1000

    make get_resource_path return same as pkg_resources

--- kcwidrp/core/kcwi_pkg_resources.py
+++ kcwidrp/core/kcwi_pkg_resources.py
@@ -6,6 +6,6 @@ def get_resource_path(pkg, path):
     ref = importlib_resources.files(pkg) / path
     with importlib_resources.as_file(ref) as full_path:
         if os.path.exists(full_path):
-            return full_path
+            return str(full_path)
         else:
             raise FileNotFoundError(f"Resource file {path} not found in package {pkg}.")
