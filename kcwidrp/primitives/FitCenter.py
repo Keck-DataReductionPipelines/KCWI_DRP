050b84b7b3a10c668941e9bf57dd4d2fd50fb412
Max Brodheim <max.brodheim@gmail.com>
2026-03-03 09:37:18 -1000

    fix taperfrac/tuckeyalpha reference bug

--- kcwidrp/primitives/FitCenter.py
+++ kcwidrp/primitives/FitCenter.py
@@ -128,7 +128,7 @@ def bar_fit_helper(argument):
         ref_flux_of_sub_spectrum = argument['reflux'][minrw:maxrw].copy()
         # get bell cosine taper to avoid nasty edge effects
         tkwgt = signal.windows.tukey(len(ref_flux_of_sub_spectrum),
-                                     alpha=argument['taperfrac'])
+                                     alpha=argument['tuckeyalpha'])
         # apply taper to atlas spectrum
         ref_flux_of_sub_spectrum *= tkwgt
         # adjust wavelengths
@@ -209,7 +209,7 @@ class FitCenter(BasePrimitive):
     and rough offset between reference bar and atlas spectrum, and the
     calculated dispersion.
 
-    Uses config parameter TAPERFRAC to control cross-correlation roll-off.
+    Uses config parameter TUCKEYALPHA to control cross-correlation roll-off.
 
     """
 
@@ -266,11 +266,11 @@ class FitCenter(BasePrimitive):
         subxvals = self.action.args.xvals[
                    self.action.args.minrow:self.action.args.maxrow]
 
-        # log taperfrac: important!
-        self.logger.info("Using TAPERFRAC = %.3f" %
-                         self.config.instrument.TAPERFRAC)
-        self.action.args.ccddata.header['TAPFRAC'] = (
-            self.config.instrument.TAPERFRAC, "taper fraction for central fit")
+        # log tuckeyalpha: important!
+        self.logger.info("Using TUCKEYALPHA = %.3f" %
+                         self.config.instrument.TUCKEYALPHA)
+        self.action.args.ccddata.header['TUCKALPH'] = (
+            self.config.instrument.TUCKEYALPHA, "taper fraction for central fit")
         # loop over bars and assemble input arguments
         my_arguments = []
         for b, bs in enumerate(self.context.arcs):
@@ -288,7 +288,7 @@ class FitCenter(BasePrimitive):
                 'xvals': self.action.args.xvals,
                 'refwave': self.action.args.refwave,
                 'reflux': self.action.args.reflux,
-                'taperfrac': self.config.instrument.TAPERFRAC,
+                'tuckeyalpha': self.config.instrument.TUCKEYALPHA,
                 'refdisp': self.action.args.refdisp,
                 'subxvals': subxvals,
                 'nn': number_of_values_to_try, 'x0': self.action.args.x0
